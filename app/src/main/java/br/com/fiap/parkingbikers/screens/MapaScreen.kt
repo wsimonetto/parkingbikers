2c070b5dcc231391ec60ccbbe5870d4766b55b6d
Wagner Machado <wagnersimonetto@gmail.com>
2024-03-08 17:27:12 -0300

    add search stations undergorund map

--- app/src/main/java/br/com/fiap/parkingbikers/screens/MapaScreen.kt
+++ app/src/main/java/br/com/fiap/parkingbikers/screens/MapaScreen.kt
@@ -21,7 +21,6 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Close
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.Card
-import androidx.compose.material3.CardColors
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
