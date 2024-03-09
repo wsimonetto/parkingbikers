ba21b69f1d87cd7559e058f1405f50aead988f13
Wagner Machado <wagnersimonetto@gmail.com>
2024-03-09 19:54:36 -0300

    add MenuScreen add UserRegisterScreen add ColorsScreen add CanvasComponent

--- app/src/main/java/br/com/fiap/parkingbikers/component/ColorsScreen.kt
+++ app/src/main/java/br/com/fiap/parkingbikers/component/ColorsScreen.kt
@@ -1,10 +1,12 @@
 package br.com.fiap.parkingbikers.component
 
+import android.annotation.SuppressLint
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
 import br.com.fiap.parkingbikers.R
 
+@SuppressLint("ComposableNaming")
 @Composable
 fun ColorsScreen(): List<Color>{
     val color1 = colorResource(id = R.color.screen_three)
